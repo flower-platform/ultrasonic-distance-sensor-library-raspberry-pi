e316987d748fd51b37344d27faf48e0a048ff200
PowerUser <PowerUser@CSP49.lan>
2016-08-22 13:30:17 +0300

    Initial structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flower-platform-ultrasonic-distance-sensor-library',
+    version = '1.0',
+    description = 'Flower Platform ultrasonic distance sensor library for Raspberry Pi',
+    author = 'Crispico',
+    author_email = 'flower@flower-platform.org',
+    url='http://www.flower-platform.org/',
+    include_package_data = True,
+    packages = ['UltrasonicDistanceSensor'],
+)
